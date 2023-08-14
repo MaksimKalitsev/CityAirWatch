2d467cfb9323ed6238205d1f0dc49756b1003858
Maksim.Kalitsev <maksim.kalitsev@ukr.net>
2023-08-14 14:59:57 +0300

    add button to main screen

--- app/src/main/java/ua/zp/cityairwatch/MainActivity.kt
+++ app/src/main/java/ua/zp/cityairwatch/MainActivity.kt
@@ -1,46 +1,75 @@
 package ua.zp.cityairwatch
 
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ua.zp.cityairwatch.ui.theme.Background
 import ua.zp.cityairwatch.ui.theme.CityAirWatchTheme
+import ua.zp.cityairwatch.ui.theme.TextButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CityAirWatchTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = Background
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button()
+        }
+    }
+}
+
+@Composable
+fun Button() {
+    ElevatedButton(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Color.White)) {
+        Text(
+            text = "Watch",
+            color = TextButton,
+            fontWeight = FontWeight.Bold,
+            fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+            fontSize = 30.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CityAirWatchTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
