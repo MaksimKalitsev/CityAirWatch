05c43df925c38df5628b50ae85c97506c25c305d
Maksim.Kalitsev <maksim.kalitsev@ukr.net>
2023-08-17 12:25:40 +0300

    add check enable bluetooth and show dialog

--- app/src/main/java/ua/zp/cityairwatch/MainActivity.kt
+++ app/src/main/java/ua/zp/cityairwatch/MainActivity.kt
@@ -1,12 +1,22 @@
 package ua.zp.cityairwatch
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import dagger.hilt.android.AndroidEntryPoint
 import ua.zp.cityairwatch.presentation.Navigation
 import ua.zp.cityairwatch.ui.theme.CityAirWatchTheme
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var bluetoothAdapter: BluetoothAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +27,24 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        showBluetoothDialog()
+    }
+
+    private fun showBluetoothDialog() {
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startBluetoothIntentForResult.launch(enableBluetoothIntent)
+        }
+    }
+
+    private val startBluetoothIntentForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode != Activity.RESULT_OK) {
+                showBluetoothDialog()
+            }
+        }
 }
 
